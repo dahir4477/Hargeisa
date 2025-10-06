6430d59532be767f7abfc1e2d3bbc850b05ca8b8
dahir4477 <dahir.niman@gmail.com>
2025-10-06 09:59:13 -0700

    Add files via upload

---
+++ hargaysa/hargaysa/src/main/java/com/example/hargaysa/HargaysaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hargaysa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HargaysaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HargaysaApplication.class, args);
+	}
+
+}
---
+++ hargaysa/hargaysa/src/test/java/com/example/hargaysa/HargaysaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hargaysa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HargaysaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
